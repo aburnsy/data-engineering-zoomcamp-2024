2fea1722951a741acf8ce5166fbdb59abc4e54a3
andrewburns89@gmail.com <andrewburns89@gmail.com>
2024-02-22 09:43:55 +0000

    Fixed issue with 2.0

---
+++ Week_4/DBT/macros/get_payment_type_description.sql
@@ -0,0 +1,23 @@
+{#
+    This macro returns the description of the payment type
+#}
+{% macro get_payment_type_description(payment_type) -%}
+
+    case
+        cast({{ payment_type }} as integer)
+        when 1
+        then 'Credit card'
+        when 2
+        then 'Cash'
+        when 3
+        then 'No charge'
+        when 4
+        then 'Dispute'
+        when 5
+        then 'Unkown'
+        when 6
+        then 'Voided trip'
+        else 'EMPTY'
+    end
+
+{%- endmacro %}
